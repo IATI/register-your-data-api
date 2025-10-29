e456dbbac96cca4f3a547f0c52553ea5befaa9ff
Simon K <6615834+simon-20@users.noreply.github.com>
2025-10-29 11:47:24 +0000

    feat: perform_undo_actions utility

--- src/register_your_data_api/utilities.py
+++ src/register_your_data_api/utilities.py
@@ -1,4 +1,9 @@
+import uuid
+from typing import Any, Callable
+
 from libsuitecrm import Filter, SuiteCRM  # type: ignore
+
+from .util import Context
 
 
 def check_crm_record_exists(crm: SuiteCRM, module: str, id: str) -> bool:
@@ -20,3 +25,35 @@ def check_crm_record_exists(crm: SuiteCRM, module: str, id: str) -> bool:
     filters.equal("id", id)
     results = crm.get_records(module, filters=filters, fields=[])
     return "data" in results and len(results["data"]) == 1
+
+
+def perform_undo_actions(
+    context: Context, undo_actions: list[tuple[str, Callable[[], Any]]], func_name: str
+) -> uuid.UUID:
+    """Runs the set of undo actions
+
+    Parameters
+    ----------
+    context : Context
+        The application context
+    undo_actions : list[tuple[str, Callable[[], Any]]]
+        A list of undo actions to perform. Each action is a tuple of (description, function)
+    func_name : str
+        The name of the function where the error occurred
+
+    Returns
+    -------
+    uuid.UUID
+        The error trace ID for the logged exception
+    """
+
+    error_id = uuid.uuid4()
+
+    context.app_logger.exception(f"Unexpected error in {func_name}. Error trace id: {error_id}")
+
+    while undo_actions:
+        (undo_msg, undo_func) = undo_actions.pop()
+        context.app_logger.error(f"For error id {error_id} running undo action: {undo_msg}")
+        undo_func()
+
+    return error_id
