346c24ac9ac9c9c88999325440a2c8b31dce43ca
Simon K <6615834+simon-20@users.noreply.github.com>
2025-11-18 20:54:44 +0000

    refactor: removed as it duplicated functionality

--- src/register_your_data_api/data_handling/domain_logic.py
+++
@@ -1,5 +0,0 @@
-from .converters import SUITECRM_TO_RYD_API_REPORTING_ORG_FIELD_MAP
-
-
-def get_reporting_org_fields_to_fetch() -> list[str]:
-    return list(SUITECRM_TO_RYD_API_REPORTING_ORG_FIELD_MAP.keys())
