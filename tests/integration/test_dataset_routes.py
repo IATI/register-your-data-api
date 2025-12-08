acf878d79e050ac19f285a315c79123aac899554
Simon K <6615834+simon-20@users.noreply.github.com>
2025-12-08 10:52:19 +0000

    test: check dataset short_name validation

---
+++ tests/integration/test_dataset_routes.py
@@ -0,0 +1,48 @@
+import json
+
+import pytest
+from fastapi.testclient import TestClient
+
+from ..helpers.mocking import MockedAppAndContext
+
+
+@pytest.mark.parametrize(
+    "invalid_short_name",
+    [
+        "invalid short name!",  # contains spaces and exclamation mark
+        "invalid@name",  # contains special character '@'
+        "name/with/slash",  # contains slash
+        "name\\with\\backslash",  # contains backslash
+        "name:with:colon",  # contains colon
+        "name*with*asterisk",  # contains asterisk
+        "name?with?question",  # contains question mark)
+    ],
+)
+def test_dataset_create_with_invalid_short_name(invalid_short_name: str) -> None:
+
+    appAndContext = MockedAppAndContext()
+
+    fastAPIapp = appAndContext.get_test_app()
+
+    new_dataset = {
+        "human_readable_name": "Test Dataset 01",
+        "licence_id": "cc-by",
+        "owner_organisation_id": "552376ae-2aa7-98ab-d800-68daa9bfeb4a",
+        "short_name": invalid_short_name,
+        "source_type": "primary_source",
+        "url": "http://www.example.com/dataset01",
+        "visibility": "private",
+    }
+
+    with TestClient(fastAPIapp) as client:
+        response = client.post(
+            "/api/v1/datasets",
+            headers=appAndContext.get_valid_authorization_header(0),
+            content=json.dumps(new_dataset),
+        )
+
+        assert response.status_code == 400
+
+        response_obj = json.loads(response.content)
+
+        assert response_obj["status"] == "failed"
