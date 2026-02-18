deeb588564880655d03bcad2f8938c2adb38f3f2
Simon K <6615834+simon-20@users.noreply.github.com>
2026-02-18 09:44:27 +0000

    fix: improve error handling for missing clients
    Improve the error handling when authenticated requests
    are made with client IDs that haven't been setup in RYD
    so that more details are available in the logs. Resolves #48

--- src/register_your_data_api/dependencies.py
+++ src/register_your_data_api/dependencies.py
@@ -1,7 +1,10 @@
+import uuid
+
 import starlette.requests
 from fastapi import Security
 
 from register_your_data_api.auth import authz
+from register_your_data_api.exceptions import RYDUserException
 from register_your_data_api.util import Context
 
 
@@ -13,7 +16,20 @@ def get_suitecrm_audit_headers(
 
     context: Context = request.app.state.context
 
-    client_app_details = context.get_client_application_details(user.client_id)
+    try:
+        client_app_details = context.get_client_application_details(user.client_id)
+    except ValueError as e:
+        trace_id = uuid.uuid4()
+        raise RYDUserException(
+            user,
+            400,
+            f"trace_id: {trace_id} - details: unknown client ID",
+            f"trace_id: {trace_id} - details: {e.args[0]}",
+            (
+                "There was a problem processing your request (invalid client ID). "
+                f"Please contact IATI Support quoting trace_id: {trace_id}"
+            ),
+        ) from e
 
     return {
         "IATI-Person-ID": user.user_id_crm,
