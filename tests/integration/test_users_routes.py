bba75b4454033369168623b3eb98ca7395f26dc0
Chris Arridge <chris.arridge@opendataservices.coop>
2026-04-24 10:47:19 +0100

    test: user roles and reporting org permissions endpoints
    Adds tests of the /user/{uid}/roles endpoint for returning a list
    of user roles, and a test for the (not yet implemented)
    /user/{uid}/roles/reporting-org-permissions/{oid} endpoint.

--- tests/integration/test_users_routes.py
+++ tests/integration/test_users_routes.py
@@ -211,12 +211,12 @@ def test_update_user_role_role_updated(new_role: str) -> None:
 
         assert response.status_code == 200
 
-        role = appAndContext._mocked_context.fine_grained_auth_provider.get_user_role_for_org(
+        roles = appAndContext._mocked_context.fine_grained_auth_provider.get_user_roles_for_org(
             uuid.UUID("698e0c1f-4e80-faa9-6533-68de801d1735"), uuid.UUID("552376ae-2aa7-98ab-d800-68daa9bfeb4a")
         )
 
-        assert role is not None
-        assert role.role.name.lower() == new_role
+        assert roles
+        assert roles[0].role.name.lower() == new_role
 
 
 def test_update_user_role_role_repaired() -> None:
@@ -225,11 +225,11 @@ def test_update_user_role_role_repaired() -> None:
 
     fastAPIapp = appAndContext.get_test_app()
 
-    role = appAndContext._mocked_context.fine_grained_auth_provider.get_user_role_for_org(
+    roles = appAndContext._mocked_context.fine_grained_auth_provider.get_user_roles_for_org(
         uuid.UUID("8c51d9bf-46c2-4d1d-869b-d9e2dd63ff48"), uuid.UUID("552376ae-2aa7-98ab-d800-68daa9bfeb4a")
     )
 
-    assert role is None  # pre-check - ensure no role exists before the update attempt
+    assert len(roles) == 0  # pre-check - ensure no role exists before the update attempt
 
     with TestClient(fastAPIapp) as client:
 
@@ -241,12 +241,11 @@ def test_update_user_role_role_repaired() -> None:
 
         assert response.status_code == 200
 
-        role = appAndContext._mocked_context.fine_grained_auth_provider.get_user_role_for_org(
+        roles = appAndContext._mocked_context.fine_grained_auth_provider.get_user_roles_for_org(
             uuid.UUID("8c51d9bf-46c2-4d1d-869b-d9e2dd63ff48"), uuid.UUID("552376ae-2aa7-98ab-d800-68daa9bfeb4a")
         )
 
-        assert role is not None
-        assert role.role.name.lower() == "editor"
+        assert roles[0].role.name.lower() == "editor"
 
 
 @pytest.mark.parametrize(
@@ -372,8 +371,159 @@ def test_delete_user_role_role_delete() -> None:
 
         assert response.status_code == 200
 
-        role = appAndContext._mocked_context.fine_grained_auth_provider.get_user_role_for_org(
+        roles = appAndContext._mocked_context.fine_grained_auth_provider.get_user_roles_for_org(
             uuid.UUID("698e0c1f-4e80-faa9-6533-68de801d1735"), uuid.UUID("552376ae-2aa7-98ab-d800-68daa9bfeb4a")
         )
 
-        assert role is None
+        assert len(roles) == 0
+
+
+@pytest.mark.parametrize(
+    "logged_in_user_idx,user_id,expected_status_code",
+    [
+        (0, "698e0c1f-4e80-faa9-6533-68de801d1735", 200),  # Person One getting Person One roles
+        (0, "bea511d3-c7a7-4097-55ed-68de81e94921", 403),  # Person One getting Person Two roles
+        (0, "a1b191ee-4c12-461c-cbe1-68de8173f628", 403),  # Person One getting Person Three roles
+        (0, "7625122c-f752-40dc-a577-5cb49e13de2a", 403),  # Person One getting Person Four roles
+        (0, "b46b88bd-05e6-4cb8-8b6a-a2c47fcd666d", 403),  # Person One getting Person Five roles
+        (0, "5c633101-42be-47ac-81e7-43d6ecb503e3", 403),  # Person One getting Person Seven roles
+        (0, "1d973500-9c23-4f63-914c-c7f1d78fb756", 403),  # Person One getting non-existent user roles
+        (1, "698e0c1f-4e80-faa9-6533-68de801d1735", 403),  # Person Two getting Person One roles
+        (1, "bea511d3-c7a7-4097-55ed-68de81e94921", 200),  # Person Two getting Person Two roles
+        (1, "a1b191ee-4c12-461c-cbe1-68de8173f628", 403),  # Person Two getting Person Three roles
+        (1, "7625122c-f752-40dc-a577-5cb49e13de2a", 403),  # Person Two getting Person Four roles
+        (1, "b46b88bd-05e6-4cb8-8b6a-a2c47fcd666d", 403),  # Person Two getting Person Five roles
+        (1, "5c633101-42be-47ac-81e7-43d6ecb503e3", 403),  # Person Two getting Person Seven roles
+        (1, "1d973500-9c23-4f63-914c-c7f1d78fb756", 403),  # Person Two getting non-existent user roles
+        (2, "698e0c1f-4e80-faa9-6533-68de801d1735", 200),  # Person Three (s/admin) getting Person One roles
+        (2, "bea511d3-c7a7-4097-55ed-68de81e94921", 200),  # Person Three (s/admin) getting Person Two roles
+        (2, "a1b191ee-4c12-461c-cbe1-68de8173f628", 200),  # Person Three (s/admin) getting Person Three roles
+        (2, "7625122c-f752-40dc-a577-5cb49e13de2a", 200),  # Person Three (s/admin) getting Person Four roles
+        (2, "b46b88bd-05e6-4cb8-8b6a-a2c47fcd666d", 200),  # Person Three (s/admin) getting Person Five roles
+        (2, "5c633101-42be-47ac-81e7-43d6ecb503e3", 200),  # Person Three (s/admin) getting Person Seven roles
+        (2, "1d973500-9c23-4f63-914c-c7f1d78fb756", 404),  # Person Three (s/admin) getting non-existent user roles
+        (3, "698e0c1f-4e80-faa9-6533-68de801d1735", 403),  # Person Four getting Person One roles
+        (3, "bea511d3-c7a7-4097-55ed-68de81e94921", 403),  # Person Four getting Person Two roles
+        (3, "a1b191ee-4c12-461c-cbe1-68de8173f628", 403),  # Person Four getting Person Three roles
+        (3, "7625122c-f752-40dc-a577-5cb49e13de2a", 200),  # Person Four getting Person Four roles
+        (3, "b46b88bd-05e6-4cb8-8b6a-a2c47fcd666d", 403),  # Person Four getting Person Five roles
+        (3, "5c633101-42be-47ac-81e7-43d6ecb503e3", 403),  # Person Four getting Person Seven roles
+        (3, "1d973500-9c23-4f63-914c-c7f1d78fb756", 403),  # Person Four getting non-existent user roles
+        (4, "698e0c1f-4e80-faa9-6533-68de801d1735", 403),  # Person Five (p/admin) getting Person One roles
+        (4, "bea511d3-c7a7-4097-55ed-68de81e94921", 403),  # Person Five (p/admin) getting Person Two roles
+        (4, "a1b191ee-4c12-461c-cbe1-68de8173f628", 403),  # Person Five (p/admin) getting Person Three roles
+        (4, "7625122c-f752-40dc-a577-5cb49e13de2a", 403),  # Person Five (p/admin) getting Person Four roles
+        (4, "b46b88bd-05e6-4cb8-8b6a-a2c47fcd666d", 200),  # Person Five (p/admin) getting Person Five roles
+        (4, "5c633101-42be-47ac-81e7-43d6ecb503e3", 403),  # Person Five (p/admin) getting Person Seven roles
+        (4, "1d973500-9c23-4f63-914c-c7f1d78fb756", 403),  # Person Five (p/admin) getting non-existent user roles
+        (6, "698e0c1f-4e80-faa9-6533-68de801d1735", 500),  # Person Seven (p/admin) getting Person One roles
+        (6, "bea511d3-c7a7-4097-55ed-68de81e94921", 500),  # Person Seven (p/admin) getting Person Two roles
+        (6, "a1b191ee-4c12-461c-cbe1-68de8173f628", 500),  # Person Seven (p/admin) getting Person Three roles
+        (6, "7625122c-f752-40dc-a577-5cb49e13de2a", 500),  # Person Seven (p/admin) getting Person Four roles
+        (6, "b46b88bd-05e6-4cb8-8b6a-a2c47fcd666d", 500),  # Person Seven (p/admin) getting Person Five roles
+        (6, "5c633101-42be-47ac-81e7-43d6ecb503e3", 500),  # Person Seven (p/admin) getting Person Seven roles
+        (6, "1d973500-9c23-4f63-914c-c7f1d78fb756", 500),  # Person Seven (p/admin) getting non-existent user roles
+    ],
+)
+def test_user_role_access_permissions(logged_in_user_idx: int, user_id: str, expected_status_code: int) -> None:
+    appAndContext = MockedAppAndContext()
+
+    fastAPIapp = appAndContext.get_test_app()
+
+    with TestClient(fastAPIapp) as client:
+        response = client.get(
+            f"/api/v1/users/{user_id}/roles",
+            headers=appAndContext.get_valid_authorization_header(logged_in_user_idx),
+        )
+
+        assert response.status_code == expected_status_code
+
+
+@pytest.mark.parametrize(
+    "logged_in_user_idx,user_id,superadmin,tools,reporting_orgs",
+    [
+        (
+            0,
+            "698e0c1f-4e80-faa9-6533-68de801d1735",
+            False,
+            [],
+            {
+                "552376ae-2aa7-98ab-d800-68daa9bfeb4a": "editor",
+                "ab851a83-a384-3eb9-caf0-68db8125b067": "admin",
+            },
+        ),
+        (
+            1,
+            "bea511d3-c7a7-4097-55ed-68de81e94921",
+            False,
+            [],
+            {
+                "552376ae-2aa7-98ab-d800-68daa9bfeb4a": "admin",
+                "da17734d-3926-47ef-8563-8a1b0247ed11": "admin",
+                "0a3a9507-d674-480e-b625-7d190f4f3319": "admin",
+            },
+        ),
+        (2, "a1b191ee-4c12-461c-cbe1-68de8173f628", True, [], {}),
+        (
+            3,
+            "7625122c-f752-40dc-a577-5cb49e13de2a",
+            False,
+            [],
+            {
+                "552376ae-2aa7-98ab-d800-68daa9bfeb4a": "contributor",
+                "ab851a83-a384-3eb9-caf0-68db8125b067": "contributor_pending",
+                "0a3a9507-d674-480e-b625-7d190f4f3319": "contributor_pending",
+            },
+        ),
+        (
+            4,
+            "b46b88bd-05e6-4cb8-8b6a-a2c47fcd666d",
+            False,
+            ["3a9d5496-77f4-497d-bb77-2bda91285111", "6a2d1ca1-b9c2-4bd3-a2a5-099178d1358d"],
+            {
+                "552376ae-2aa7-98ab-d800-68daa9bfeb4a": "provider_admin",
+                "ab851a83-a384-3eb9-caf0-68db8125b067": "provider_admin",
+            },
+        ),
+    ],
+)
+def test_user_roles_correctly_returned(
+    logged_in_user_idx: int, user_id: str, superadmin: bool, tools: list[str], reporting_orgs: dict[str, str]
+) -> None:
+    appAndContext = MockedAppAndContext()
+
+    fastAPIapp = appAndContext.get_test_app()
+
+    with TestClient(fastAPIapp) as client:
+        response = client.get(
+            f"/api/v1/users/{user_id}/roles",
+            headers=appAndContext.get_valid_authorization_header(logged_in_user_idx),
+        )
+
+        assert response.status_code == 200
+
+        resp_as_object = response.json()
+
+        assert resp_as_object["data"]["superadmin"] == superadmin
+        assert len(resp_as_object["data"]["tools"]) == len(tools)
+        assert sorted(resp_as_object["data"]["tools"]) == sorted(tools)
+        assert len(resp_as_object["data"]["reporting_orgs"]) == len(reporting_orgs)
+        assert resp_as_object["data"]["reporting_orgs"] == reporting_orgs
+
+
+def test_user_role_reporting_org_permissions_not_implemented() -> None:
+    appAndContext = MockedAppAndContext()
+
+    fastAPIapp = appAndContext.get_test_app()
+
+    # When implemented, this call should return a list of permissions matching "editor".
+    with TestClient(fastAPIapp) as client:
+        response = client.get(
+            (
+                "/api/v1/users/698e0c1f-4e80-faa9-6533-68de801d1735/roles/"
+                "reporting-org-permissions/552376ae-2aa7-98ab-d800-68daa9bfeb4a"
+            ),
+            headers=appAndContext.get_valid_authorization_header(0),
+        )
+
+        assert response.status_code == 501
